04f1130b2ccb6c4250bbacb75b485b904f7c4213
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-04-19 00:00:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ const __dirname = path.dirname(__filename)
 const app = express()
 const port = process.env.PORT || 3000
 
-// Servir archivos estáticos desde /dist
 app.use(express.static(path.join(__dirname, 'dist')))
 
-// Redirigir todo lo demás a index.html (para React Router)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
 app.listen(port, () => {
-  console.log(`Servidor en http://localhost:${port}`)
+  console.log(`Servidor corriendo en http://localhost:${port}`)
 })

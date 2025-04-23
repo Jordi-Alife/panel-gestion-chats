ca4f7f725866413846f59044d5610c587e1c9728
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-04-23 21:50:02 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,12 @@
-self.addEventListener('install', () => {
+self.addEventListener('install', event => {
   console.log('Service Worker instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activado');
+});
+
+self.addEventListener('fetch', event => {
+  // Por ahora solo dejamos pasar las peticiones sin cachear nada
 });

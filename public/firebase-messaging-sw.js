169fffa705a1b60f0b3e323d923c878f8c73319f
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-04-24 09:29:36 +0200

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB0vz-jtc7PRpdFfQUKvU9PevLEV8zYzO4",
+  authDomain: "nextlives-panel-soporte.firebaseapp.com",
+  projectId: "nextlives-panel-soporte",
+  storageBucket: "nextlives-panel-soporte.firebasestorage.app",
+  messagingSenderId: "52725281576",
+  appId: "1:52725281576:web:4402c0507962074345161d"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Mensaje recibido en segundo plano:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon-192x192.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});

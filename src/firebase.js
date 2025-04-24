4f05778dc6965f6177348cc4743d17939b9ab821
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-04-24 13:31:51 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "nextlives-panel-soporte",
   storageBucket: "nextlives-panel-soporte.firebasestorage.app",
   messagingSenderId: "52725281576",
-  appId: "1:52725281576:web:4402c0507962074345161d"
+  appId: "1:52725281576:web:4402c0507962074345161d",
 };
 
 // Inicializa Firebase
@@ -18,17 +18,22 @@ const VAPID_KEY = "BGBob8bXua7_QSiRd_QHLp6ZvwSRN2gq00Fm8VGk4CbquXL28qa8y-pPevdP7
 
 // Obtener token para notificaciones push
 export async function obtenerToken() {
-  if (typeof window === "undefined") return null;
+  if (typeof window === 'undefined') return null;
 
-  const { getMessaging, getToken, isSupported } = await import('firebase/messaging');
-  const soportado = await isSupported();
-  if (!soportado) return null;
-
-  const messaging = getMessaging(app);
   try {
+    const { getMessaging, getToken, isSupported } = await import('firebase/messaging');
+    const soportado = await isSupported();
+    if (!soportado) return null;
+
+    const messaging = getMessaging(app);
     const token = await getToken(messaging, { vapidKey: VAPID_KEY });
-    console.log("🔐 Token:", token);
-    return token;
+    if (token) {
+      console.log("🔐 Token:", token);
+      return token;
+    } else {
+      console.warn("⚠️ No se obtuvo el token.");
+      return null;
+    }
   } catch (err) {
     console.error("❌ Error al obtener token:", err);
     return null;
@@ -37,12 +42,16 @@ export async function obtenerToken() {
 
 // Escuchar mensajes en primer plano
 export async function escucharMensajes(callback) {
-  if (typeof window === "undefined") return;
+  if (typeof window === 'undefined') return;
 
-  const { getMessaging, onMessage, isSupported } = await import('firebase/messaging');
-  const soportado = await isSupported();
-  if (!soportado) return;
+  try {
+    const { getMessaging, onMessage, isSupported } = await import('firebase/messaging');
+    const soportado = await isSupported();
+    if (!soportado) return;
 
-  const messaging = getMessaging(app);
-  onMessage(messaging, callback);
+    const messaging = getMessaging(app);
+    onMessage(messaging, callback);
+  } catch (err) {
+    console.error("❌ Error al escuchar mensajes:", err);
+  }
 }

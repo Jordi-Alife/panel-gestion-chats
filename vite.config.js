5e35b257cf66e7edd4941a87bd9ce3778e7b002c
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-04-24 16:34:21 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // ← clave para rutas relativas correctas en producción
+  base: './',
   plugins: [react()],
   css: {
     preprocessorOptions: {

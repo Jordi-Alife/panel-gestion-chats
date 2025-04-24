fa65ffe6dc1be265234471588742e7f93fbae3cd
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-04-24 10:36:52 +0200

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -21,4 +22,4 @@ export default defineConfig({
     host: true,
     port: 3000
   }
-});
+})

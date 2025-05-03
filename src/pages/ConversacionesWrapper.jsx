03b67621d440d6950c662ccdc87f2917e7f168a6
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-03 17:25:15 +0200

    Update ConversacionesWrapper.jsx

--- src/pages/ConversacionesWrapper.jsx
+++ src/pages/ConversacionesWrapper.jsx
@@ -1,1 +1,8 @@
+import ChatMovil from "./ChatMovil";
+import Conversaciones from "./Conversaciones"; // si tu archivo original sigue llamándose Conversaciones.jsx
 
+export default function ConversacionesWrapper() {
+  const isMobile = window.innerWidth < 768;
+  
+  return isMobile ? <ChatMovil /> : <Conversaciones />;
+}

4e6c3e65a6d9a0bee64d7166b2bf4024289eb215
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-08 14:24:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ import Agentes from "./pages/agentes";
 import AgenteDetalle from "./pages/AgenteDetalle";
 import Perfil from "./pages/Perfil";
 import Login from "./pages/Login";
-import Inicio from "./pages/Inicio";
-import Monitor from "./pages/Monitor"; // ✅ nueva importación añadida
+import Notificaciones from "./components/Notificaciones";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { app } from "./firebaseAuth";
+import Inicio from "./pages/Inicio";
 
 const App = () => {
   const [usuarioActual, setUsuarioActual] = useState(null);
@@ -97,7 +97,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
 
-        {/* ✅ ChatMovil fuera del DashboardLayout */}
+        {/* ✅ ChatMovil FUERA del DashboardLayout */}
         <Route
           path="/conversaciones/:userId"
           element={usuarioActual ? <ChatMovil /> : <Navigate to="/login" />}
@@ -114,7 +114,6 @@ const App = () => {
                   <Route path="/agentes" element={<Agentes />} />
                   <Route path="/agente/:uid" element={<AgenteDetalle />} />
                   <Route path="/perfil" element={<Perfil />} />
-                  <Route path="/monitor" element={<Monitor />} /> {/* ✅ nueva ruta monitor */}
                 </Routes>
               </DashboardLayout>
             ) : (

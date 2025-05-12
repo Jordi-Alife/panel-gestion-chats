211ab0c9a92275645949e3bd463f2b3780b25a75
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-12 20:07:44 +0200

    Update Conversaciones.jsx

--- src/pages/Conversaciones.jsx
+++ src/pages/Conversaciones.jsx
@@ -250,7 +250,7 @@ export default function Conversaciones() {
   );
 
   return (
-    <div className="flex flex-row h-screen bg-[#f0f4f8] overflow-hidden">
+    <div className="flex flex-row h-[calc(100vh-32px)] bg-[#f0f4f8] overflow-hidden">
       <div className="w-1/5 h-full overflow-y-auto">
         <ConversacionList
           conversaciones={listaAgrupada}

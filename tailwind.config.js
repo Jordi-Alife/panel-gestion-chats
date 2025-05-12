3e438169ebf1f60cd25881cf5af4500d6f9af966
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-12 22:50:40 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // ✅ Activamos el modo oscuro basado en clase
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},

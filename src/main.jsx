c9037e6bfbac54951c04ae04461a3b654d46a706
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-14 23:42:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// ✅ Activar clase "dark" si está activado en localStorage
+const dark = localStorage.getItem("modo-oscuro") === "true"
+if (dark) {
+  document.documentElement.classList.add("dark")
+} else {
+  document.documentElement.classList.remove("dark")
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

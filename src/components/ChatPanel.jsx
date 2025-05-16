76b9f0ca4bb4502be6177ca5b7020815051b2ab1
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-16 23:26:26 +0200

    Update ChatPanel.jsx

--- src/components/ChatPanel.jsx
+++ src/components/ChatPanel.jsx
@@ -92,7 +92,7 @@ const ChatPanel = ({
 
           return (
             <div key={index} data-id={msg.id} className={`flex ${align}`}>
-              <div className={`max-w-[70%] p-3 shadow ${shapeClass} ${
+              <div className={`max-w-[70%] p-3 ${shapeClass} ${
                 msg.manual
                   ? "bg-[#2563eb] text-white"
                   : isAsistente

3fbc6c2c8e6019fdec9a82f7aa017483788a24ec
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-05-29 09:53:08 +0200

    Update Conversaciones.jsx

--- src/pages/Conversaciones.jsx
+++ src/pages/Conversaciones.jsx
@@ -157,7 +157,7 @@ export default function Conversaciones() {
 useEffect(() => {
   if (tipoVisualizacion === "archivadas") {
     console.log("📦 Cargando archivadas");
-    cargarDatos("archivadas");
+    cargarDatos("archivo");
     return; // no hay refresco
   }
 

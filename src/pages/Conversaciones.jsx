889d243b858bd6293ba610e811a8b49500e593ab
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-06-02 21:00:14 +0200

    Update Conversaciones.jsx

--- src/pages/Conversaciones.jsx
+++ src/pages/Conversaciones.jsx
@@ -23,7 +23,7 @@ export default function Conversaciones() {
   const [vistas, setVistas] = useState({});
   const [mostrarScrollBtn, setMostrarScrollBtn] = useState(false);
   const [filtro, setFiltro] = useState("todas");
-  const [tipoVisualizacion, setTipoVisualizacion] = useState("archivadas");
+  const [tipoVisualizacion, setTipoVisualizacion] = useState("recientes");
   const [agente, setAgente] = useState(null);
   const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null);
   const [textoEscribiendo, setTextoEscribiendo] = useState("");

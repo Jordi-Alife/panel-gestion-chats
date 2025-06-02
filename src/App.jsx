4587194ead8a3cd8f0747b6b103e66c6a0d7b426
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-06-02 13:30:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import PerfilMovil from "./pages/PerfilMovil";
 import Login from "./pages/Login";
 import Inicio from "./pages/Inicio";
 import Monitor from "./pages/Monitor";
+import ArchivadasNew from "./pages/ArchivadasNew"; // ✅ Import del nuevo archivo
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { app } from "./firebaseAuth";

c623bb0f7fc7aab9696c499a1762a75edbcc84f5
Jordi MM <71398263+Jordi-Alife@users.noreply.github.com>
2025-06-16 13:41:56 +0200

    Update ChatMovil.jsx

--- src/pages/ChatMovil.jsx
+++ src/pages/ChatMovil.jsx
@@ -572,9 +572,12 @@ setEnviando(true);
             style={{ fontSize: "16px" }}
           />
           <button
-            type="submit"
-            className="bg-blue-600 text-white rounded-full w-10 h-10 flex items-center justify-center"
-          >
+  type="submit"
+  disabled={enviando}
+  className={`bg-blue-600 text-white rounded-full w-10 h-10 flex items-center justify-center ${
+    enviando ? "opacity-50 cursor-not-allowed" : ""
+  }`}
+>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M5 10l7-7m0 0l7 7m-7-7v18" />
             </svg>
